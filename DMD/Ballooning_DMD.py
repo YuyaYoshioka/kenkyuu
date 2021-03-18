8f3bea949b008e52ea144ef9e02b57de29c13b25
YuyaYoshioka <yuya3330930@yahoo.co.jp>
2021-03-18 19:02:28 +0900

    コメントを追記

--- DMD/Ballooning_DMD.py
+++ DMD/Ballooning_DMD.py
@@ -14,8 +14,8 @@ import math
 from decimal import Decimal, ROUND_HALF_UP, ROUND_HALF_EVEN
 import os
 
+# 動的モード分解するファイルを指定
 path = os.getcwd()
-
 file_list = glob.glob('04P????.txt')
 
 u=[]
@@ -40,7 +40,6 @@ for u_ in u:
         number+=3
     U.append(n_u)
 
-print(R[:100])
 
 U_n=np.array(U)
 U_n_T=U_n.T
@@ -51,7 +50,7 @@ X1=U_n_T[:,1:]
 mode=np.arange(1,21,1)
 U,Sig,Vh2 = svd(X0, False)
 
-print(Sig)
+# 固有値を計算
 plt.xlabel('mode')
 plt.ylabel('eigenvalues')
 plt.plot(mode,Sig[:20],linestyle='None',marker='.')
@@ -59,6 +58,7 @@ plt.plot(mode,Sig[:20],linestyle='None',marker='.')
 
 values=[]
 
+# 累積寄与率を計算
 whole=sum(Sig)
 for n in range(20):
   value=Sig[:n+1]
@@ -70,8 +70,10 @@ plt.plot(mode,values,linestyle='None',marker='.')
 # plt.show()
 
 
-
+# モード数を指定
 r=10
+
+
 Sig_r=np.eye(r)
 V = Vh2.conj().T[:,:r]
 for a in range(r):
@@ -94,6 +96,8 @@ for i,_t in enumerate(t):
 n_f = dot(phi, Psi)
 f=U_n_T
 
+
+#　成長率及び周波数を計算
 tans=[]
 for r_ in range(r):
     tan=[]
@@ -139,6 +143,8 @@ for n in range(0,len(ganmas),2):
 
 # plt.show()
 
+# 動的モード分解後のモードのファイルを出力
+
 for r_ in range(0,r):
     path_w = path+'/'+str(r_)+'.txt'
     number=1
@@ -181,21 +187,3 @@ for r_ in range(0,r):
         number+=3
     with open(path_w, mode='w') as f:
         f.write('\n'.join(Out))
-
-R=np.array([0.781, 2.344, 3.906, 5.469, 7.031, 8.594, 10.156, 11.719, 13.281, 14.844, 16.406, 17.969, 19.531, 21.094, 22.656, 24.219, 25.781, 27.344, 28.906, 30.469, 32.031, 33.594, 35.156, 36.719, 38.281, 39.844, 41.406, 42.969, 44.531, 46.094, 47.656, 49.219, 50.781, 52.344, 53.906, 55.469, 57.031, 58.594, 60.156, 61.719, 63.281, 64.844, 66.406, 67.969, 69.531, 71.094, 72.656, 74.219, 75.781, 77.344, 78.906, 80.469, 82.031, 83.594, 85.156, 86.719, 88.281, 89.844, 91.406, 92.969, 94.531, 96.094, 97.656, 99.219, 100.781, 102.344, 103.906, 105.469, 107.031, 108.594, 110.156, 111.719, 113.281, 114.844, 116.406, 117.969, 119.531, 121.094, 122.656, 124.219, 125.781, 127.344, 128.906, 130.469, 132.031, 133.594, 135.156, 136.719, 138.281, 139.844, 141.406, 142.969, 144.531, 146.094, 147.656, 149.219])
-
-
-fig = plt.figure()
-for r_ in range(0,6,2):
-    Rs = []
-    for n in range(len(R)):
-        rs=0
-        for n_ in range(n,len(phi[:,r_]),len(R)):
-            rs+=abs(phi[:,r_][n_])
-        Rs.append(rs/(len(phi[:,r_])//len(R)))
-    plt.rcParams["font.size"] = 14
-    plt.tight_layout()
-    plt.xlabel('r')
-    plt.plot(R,Rs,label='m='+str(r_+1)+','+str(r_+2))
-    plt.legend()
-fig.savefig("R.png")
